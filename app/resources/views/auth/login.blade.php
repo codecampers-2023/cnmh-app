9ad84d04b7acac1a73ce12e185a9c03bba4eb7d4
El-Mliki-Hicham <elmliki.hicham@gmail.com>
2023-06-20 04:21:46 +0200

    login mzowra

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     <body class="hold-transition login-page">
         <div class="login-box">
             <div class="login-logo">
-                <a href="{{ url('/home') }}"><b>{{ config('app.name') }}</b></a>
+                <a href="{{ url('/home') }}"><b>CNMH</b></a>
             </div>
             <!-- /.login-logo -->
 
@@ -12,22 +12,22 @@
                 <div class="card-body login-card-body">
                     <p class="login-box-msg">Sign in to start your session</p>
 
-                    <form method="post" action="{{ url('/login') }}">
+                    <form method="post" action="{{ route('login') }}">
                         @csrf
 
                         <div class="input-group mb-3">
-                            <input type="email" name="email" value="{{ old('email') }}" placeholder="Email"
-                                class="form-control @error('email') is-invalid @enderror">
+                            <input type="name" name="name" value="admin" placeholder="name"
+                                class="form-control @error('name') is-invalid @enderror">
                             <div class="input-group-append">
                                 <div class="input-group-text"><span class="fas fa-envelope"></span></div>
                             </div>
-                            @error('email')
+                            @error('error')
                                 <span class="error invalid-feedback">{{ $message }}</span>
                             @enderror
                         </div>
 
                         <div class="input-group mb-3">
-                            <input type="password" name="password" placeholder="Password"
+                            <input type="password" name="password" value="admin" placeholder="Password"
                                 class="form-control @error('password') is-invalid @enderror">
                             <div class="input-group-append">
                                 <div class="input-group-text">
@@ -54,13 +54,6 @@
 
                         </div>
                     </form>
-
-                    <p class="mb-1">
-                        <a href="{{ route('password.request') }}">I forgot my password</a>
-                    </p>
-                    <p class="mb-0">
-                        <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
-                    </p>
                 </div>
                 <!-- /.login-card-body -->
             </div>
